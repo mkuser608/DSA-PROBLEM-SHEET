8b1e46bed01090ca93f238b3ce60c394fe288a99
M K Mandal <mkuser608@gmail.com>
2024-05-31 07:39:03 +0530

    navbar fixwd

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -1,17 +1,17 @@
-// components/Navbar.tsx
 import React from "react";
 import ThemeSwitcher from "./ThemeSwitcher";
 
 const Navbar = () => {
   return (
-    <nav className="w-full bg-gray-200 dark:bg-gray-800 p-4 flex justify-between items-center opacity-60 sticky top-0 ">
-       <div className="text-2xl font-bold text-gray-900 dark:text-white">
-        DSA 450 Questions
-      </div>
-      <ThemeSwitcher />
-    </nav>
+    <div className="bg-gray-200/70 dark:bg-gray-800/70 backdrop-blur-none sticky top-0  w-full  z-50">
+      <nav className="w-full p-4 flex justify-between items-center backdrop-blur-md">
+        <div className="text-2xl font-bold text-gray-900 dark:text-white">
+          DSA 450 Questions
+        </div>
+        <ThemeSwitcher />
+      </nav>
+    </div>
   );
 };
 
 export default Navbar;
- 
--- app/components/ui/bento-grid.tsx
+++ app/components/ui/bento-grid.tsx
@@ -10,7 +10,7 @@ export const BentoGrid = ({
   return (
     <div
       className={cn(
-        "grid md:auto-rows-[16rem] grid-cols-1 sm:grid-cols-2 md:grid-cols-3  gap-4 max-w-7xl mx-auto ",
+        "grid md:auto-rows-[16rem] grid-cols-1 sm:grid-cols-2 md:grid-cols-3  gap-4 max-w-7xl mx-auto  ",
         className
       )}
     >
@@ -38,7 +38,7 @@ export const BentoGridItem = ({
     <>
       <div
         className={cn(
-          "row-span-1 rounded-xl group/bento  transition duration-200 dark:shadow-none p-4 border-transparent justify-between flex flex-col  space-y-4",
+          " row-span-1 rounded-xl group/bento  transition duration-200 dark:shadow-none p-4 border-transparent justify-between flex flex-col  space-y-4",
           className
         )}
       >
