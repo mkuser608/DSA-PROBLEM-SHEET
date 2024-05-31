efa650de78e675c52d72c3c5e86265ec0b0c9ce6
M K Mandal <mkuser608@gmail.com>
2024-05-31 06:54:33 +0530

    data fetch issue solved

--- app/components/grids/TopicCardBentGrid.tsx
+++ app/components/grids/TopicCardBentGrid.tsx
@@ -1,40 +1,11 @@
 "use client";
 import React from "react";
 import TopicCard from "../Cards/Topic-Card";
-import { dataSchema } from "../../utils/ValidationData";
 import data from "../../data/450DSA";
 import { BentoGrid, BentoGridItem } from "../ui/bento-grid";
 
-interface Question {
-  Topic: string;
-  Problem: string;
-  Done: boolean;
-  Bookmark: boolean;
-  Notes?: string;
-  URLs: string[];
-}
-
-interface Topic {
-  topicName: string;
-  position: number;
-  started: boolean;
-  doneQuestions: number;
-  questions: Question[];
-}
-
-async function getData(): Promise<Topic[]> {
-  const validationResult = dataSchema.safeParse(data);
-
-  if (!validationResult.success) {
-    console.error("Validation failed:", validationResult.error.errors);
-    throw new Error("Data validation failed");
-  }
-
-  return validationResult.data;
-}
-
-const TopicCardBentGrid = async () => {
-  const topics = await getData();
+const TopicCardBentGrid = () => {
+  const topics = data;
 
   return (
     <div>
